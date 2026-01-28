dd57522d373df3e41114bc34da5647d11eca0220
Gargi Balamwar <142029062+GargiBalamwar@users.noreply.github.com>
2026-01-28 21:05:52 +0530

    Add trigger to increment assigned parcels on insert

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,1 +1,10 @@
+CREATE OR REPLACE TRIGGER trg_increment_assigned_parcels
+AFTER INSERT ON DeliveryStatus
+FOR EACH ROW
+BEGIN
+    UPDATE Employee
+    SET AssignedParcels = AssignedParcels + 1
+    WHERE EmployeeID = :NEW.EmployeeID;
+END;
+/
 
