6225aa7baaea39b263070314a48d736a97002795
Gargi Balamwar <142029062+GargiBalamwar@users.noreply.github.com>
2026-01-28 21:05:18 +0530

    Add SQL queries for updating, altering, and selecting

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,12 @@
+-- UPDATE Query
+UPDATE DeliveryStatus
+SET Status = 'Delivered', Timestamp = SYSDATE
+WHERE ParcelID = 3;
+
+-- ALTER Query
+ALTER TABLE Customer
+ADD EmailVerified VARCHAR2(5);
+
+-- SELECT Query
+SELECT * FROM DeliveryStatus;
 
